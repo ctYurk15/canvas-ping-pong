000857c7a2a1cdd7c5b9163f664ab9e7e3774cdb
ctYurk15 <levgenetic@gmail.com>
2022-06-25 20:54:06 +0300

    Display scores

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 const canvas = document.querySelector('canvas');
+const scoreTables = document.querySelectorAll(".score-text");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const engine = new Engine(canvas, 'rgb(200, 255, 255)');
+
+const scores = [0, 0];
 
 const rocket_margin = 20;
 const rocket_speed = 5;
@@ -12,7 +15,6 @@ const start_rocket_y = canvas.height/2-rocket_height/2;
 
 const ball_radius = 20;
 const ball_start_direction = {x: -1, y: 1};
-const scores = [0, 0];
 
 const rocket1 = new Rocket(rocket_margin, start_rocket_y, rocket_width, rocket_height, 'red');
 const rocket2 = new Rocket(canvas.width - rocket_width - rocket_margin, start_rocket_y, rocket_width, rocket_height, 'blue');
@@ -20,6 +22,9 @@ const ball = new Ball(canvas.width/2, canvas.height/2, ball_radius, 'green', 2, 
 ball.setDirection(ball_start_direction);
 ball.setGatesCollidAction(function(gate_index){
     scores[gate_index]++;
+
+    scoreTables[0].innerHTML = scores[0];
+    scoreTables[1].innerHTML = scores[1];
 
     ball.x = canvas.width/2;
     ball.y = canvas.height/2;
